a2a7ac1ca570954a50015066730e2a9011e3ae9e
Miguh Ruiz <miguhruiz@gmail.com>
2017-12-02 20:47:38 +0100

    [Init] Electron init

---
+++ index.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron')
+
+let win
+
+app.on('ready', () => {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            devTools: true
+        }
+    })
+
+    win.loadURL(`file://${__dirname}/static/index.html`)
+})
